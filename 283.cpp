e15e593564f0b6485f2b5dbf2abc30d4d68d4d10
Wasyl <WasylFranchuk@gmail.com>
2016-08-03 12:26:45 +0300

    Move Zeroes

---
+++ 283.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+            if (nums[i])
+                nums[j++] = nums[i];
+                
+        for (int i = j; i < n; i++)
+            nums[i] = 0;
+    }
+};
