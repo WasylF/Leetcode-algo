44b8a3e62c7338f3814fb77cf664fa499c5da2a3
Wasyl <WasylFranchuk@gmail.com>
2016-08-03 12:06:37 +0300

    Reverse String

---
+++ 344.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string reverseString(string s) {
+     string ans;
+     ans.reserve(s.length());
+     for (int i = s.length() - 1; i >= 0; i--)
+        ans += s[i];
+        
+     return ans;
+    }
+};
