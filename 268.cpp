dd03e976cce173b2b7271107054828437d0b6e06
Wasyl <WasylFranchuk@gmail.com>
2016-08-05 14:19:21 +0300

    Missing Number

---
+++ 268.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xr = 0;
+        for (int i = 0; i<=n; i++)
+            xr ^= i;
+        
+        for (int num : nums)
+            xr ^= num;
+            
+        return xr;
+    }
+};
